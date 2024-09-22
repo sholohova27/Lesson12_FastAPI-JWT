f7c708e2b76a105ada40bd0a35973a272e3642fb
Nataly Sholohova <83698795+sholohova27@users.noreply.github.com>
2024-09-22 20:55:53 +0300

    Add files via upload

--- src/database/db.py
+++ src/database/db.py
@@ -1,8 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "postgresql+psycopg2://nataly:123456@localhost:5433/my-postgres"
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
